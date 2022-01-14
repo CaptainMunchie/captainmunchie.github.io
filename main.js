3345b898253ba104f0ecaf19e683b2d950f60489
https://github.com/captainmunchies <jeffrey.song@students.mq.edu.au>
2022-01-14 18:44:42 +1100

    project blog

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+
+// Popup Box
+const serviceItems = document.querySelector(".project_container");
+const popup = document.querySelector(".popup-box")
+// const popupCloseBtn = popup.querySelector(".popup-close-btn");
+const popupCloseIcon = popup.querySelector(".popup-close-icon");
+
+serviceItems.addEventListener("click",function(event){
+   if(event.target.tagName.toLowerCase() == "button"){
+      const item =event.target.parentElement.parentElement;
+      console.log(item);
+      const h3 = item.querySelector("h3").innerHTML;
+      
+      const readMoreCont = item.querySelector(".read-more-content").innerHTML;
+      popup.querySelector("h3").innerHTML = h3;
+      popup.querySelector(".popup-body").innerHTML = readMoreCont;
+      popupBox();
+   }
+
+})
+
+// popupCloseBtn.addEventListener("click", popupBox);
+popupCloseIcon.addEventListener("click", popupBox);
+
+popup.addEventListener("click", function(event){
+   if(event.target == popup){
+      popupBox();
+   }
+})
+
+function popupBox(){
+   popup.classList.toggle("open");
+}
+
 /*===== MENU SHOW Y HIDDEN =====*/ 
 const navMenu = document.getElementById('nav-menu'),
     toggleMenu = document.getElementById('nav-toggle'),
